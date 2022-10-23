5b8fbf1fd747be9f65bd073fb2c530bed996892e
Andrés Sánchez <sitosanzdev@gmail.com>
2022-10-24 00:46:52 +0200

    V5.5

--- prueba.sh
+++ prueba.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
-read num1
-read num2
-echo ""
 
-function sequensiaSuma {
-    res=0
-    # Bucle:
+echo "1) Suma
+2) Resta
+3) Salir"
 
-    for var in $(seq $num1 $num2); do
-        res=$(expr $res + $var)
-    done
-    # Resultado:
-    echo "El resultado es $res."
+function operacionSumar {
+    read num1
+    read num2
+    var=$(($num1 + $num2))
+    echo $var
 }
 
-function sequensia {
-    seq -s "+" $num1 $num2
+function operacionRestar {
+    read num1
+    read num2
+    var=$(($num1 - $num2))
+    echo $var
 }
 
-function vuelti {
-    if [ "$num2" -lt "$num1" ]; then
-        echo "El numero 2 es menor bro"
-        alt1=$num2
-        alt2=$num1
-        echo "$alt1"
-        echo "$alt2"
-
-        res=0
-        # Bucle:
-
-        for var in $(seq $alt1 $alt2); do
-            res=$(expr $res + $var)
-        done
-        # Resultado:
-        echo "El resultado es $res."
-    fi
-}
-
-# sequensiaSuma
-# sequensia
-vuelti
+while true; do
+    echo "Dame una opc"
+    read opc
+    case $opc in
+    1)
+        echo "Suma"
+        operacionSumar
+        exit
+        ;;
+    2)
+        echo "Resta"
+        operacionRestar
+        exit
+        ;;
+    3)
+        echo "Salir"
+        exit
+        ;;
+    *)
+        echo "Elige una opción válida. Si quieres salir, pulsa 3"
+        ;;
+    esac
+done
